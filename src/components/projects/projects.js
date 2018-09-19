7fe3e961195e4429dbc2fbce89629bbccafd6654
MartynFung <mknfung@gmail.com>
2018-09-19 00:32:48 -0700

    adjusted project description

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -17,18 +17,18 @@ export default class Projects extends React.Component {
                                 <a target="_blank" href="http://eleveight.co/">
                                     <div className="card-img" style={{ backgroundImage: `url(images/portfolio/eleveight.png)` }}></div>
                                 </a>
-                                    <div className="card-content">
-                                        <div className="card-tags">
-                                            <div className="card-tag text-sm">Work</div>
-                                            <div className="card-tag text-sm">C#</div>
-                                            <div className="card-tag text-sm">.NET</div>
-                                            <div className="card-tag text-sm">SQL</div>
-                                            <div className="card-tag text-sm">React.js</div>
-                                        </div>
-                                        <h4>Eleveight</h4>
-                                        <p>Created an MVP web application for Eleveight utilizing C#, .NET, Web API, React.JS, HTML, CSS, Bootstrap</p>
-                                        <a rel="noopener noreferrer" target="_blank" href="https://github.com/MartynFung/work_samples"><strong>View Code</strong></a>
+                                <div className="card-content">
+                                    <div className="card-tags">
+                                        <div className="card-tag text-sm">Work</div>
+                                        <div className="card-tag text-sm">C#</div>
+                                        <div className="card-tag text-sm">.NET</div>
+                                        <div className="card-tag text-sm">SQL</div>
+                                        <div className="card-tag text-sm">React.js</div>
                                     </div>
+                                    <h4>Eleveight</h4>
+                                    <p>Created an MVP web application for Eleveight utilizing C#, .NET, Web API, React.JS, HTML, CSS, Bootstrap</p>
+                                    <a rel="noopener noreferrer" target="_blank" href="https://github.com/MartynFung/work_samples"><strong>View Code</strong></a>
+                                </div>
                             </div>
                         </div>
 
@@ -43,7 +43,7 @@ export default class Projects extends React.Component {
                                         <div className="card-tag text-sm">CSS</div>
                                     </div>
                                     <h4>Portfolio Page</h4>
-                                    <p>Designed a personal portfolio page using React.js, HTML, CSS. And many more things that sound cool.</p>
+                                    <p>Designed a personal portfolio page using React.js, HTML, CSS. Currently in progress.</p>
                                     <a rel="noopener noreferrer" target="_blank" href=""><strong>In Progress</strong></a>
                                 </div>
                             </div>
